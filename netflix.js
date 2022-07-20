d67bdc38bb5d6086f24339d9d0506fe7c97b1ead
Chris Fink <65560083+chris-fink@users.noreply.github.com>
2022-07-20 15:54:53 -0400

    Create netflix.js

---
+++ netflix.js
@@ -0,0 +1,11 @@
+import Button from "https://cdn.skypack.dev/react-bootstrap@2.4.0";
+
+function selectItems() {
+  return (
+    <>
+      <Button variant="danger">Sign In</Button>{' '}
+    </>
+  );
+}
+
+export default TypesExample;
